23439ae5858e4c8819e4efdddf5d43c11645c987
Alexandra Smith <smithalexwork@gmail.com>
2024-09-28 09:40:49 -0400

    o(n) & o(1) notation practice

--- src/TimeComplexity.java
+++ src/TimeComplexity.java
@@ -1,2 +1,21 @@
 public class TimeComplexity {
+    // 0(1) - constant time. ex: accessing first element in an array
+    public static int getFirstElement(int[] array) {
+        return array[0];
+    }
+
+    //0(n) linear growth time: adding the sum of an array
+    public static int sumArray(int [] array) {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++){
+            sum += array[i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int [] array = {15, 30, 45, 60, 75};
+        System.out.println("The sum of the array is " + sumArray(array));
+        System.out.println("The first element of the array is " + getFirstElement(array));
+    }
 }
